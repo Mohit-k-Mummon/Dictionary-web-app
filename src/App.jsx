80c52953232c5adecefd9e84c062d9b0ae58022b
Mohit-k-Mummon <mxkumar@protonmail.com>
2023-01-21 12:33:46 -0800

    init commit / nav component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Nav from './components/Nav';
+
+function App() {
+	return (
+		<React.Fragment>
+			<Nav />
+		</React.Fragment>
+	);
+}
+
+export default App;
